1ded77cc8bcc440baceb6b63611eba4c80af4cb5
Oleksandr Malomed <alexandrstrum@gmail.com>
2020-04-15 20:43:14 +0400

    Add project files.

---
+++ ToLower/ToLowerModule.cs
@@ -0,0 +1,12 @@
+﻿using ExtensionAdapter;
+
+namespace ToLower {
+    public class ToLowerModule : IFunctionModule {
+        public FunctionInfo GetModuleFunctionInfo() {
+            return new FunctionInfo {
+                ButtonText = "To lower",
+                Function = (input) => input.ToLower()
+            };
+        }
+    }
+}
---
+++ ToUpper/ToUpperModule.cs
@@ -0,0 +1,16 @@
+﻿using ExtensionAdapter;
+
+namespace ToUpper {
+    public class ToLowerModule : IFunctionModule {
+        public FunctionInfo GetModuleFunctionInfo() {
+            return new FunctionInfo {
+                ButtonText = "To upper",
+                Function = ProcessInput
+            };
+        }
+
+        private string ProcessInput(string arg) {
+            return arg.ToLower();
+        }
+    }
+}
